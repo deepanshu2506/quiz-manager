f7c5fb4d730b2c63fb58fa2e43f1ee86868458b4
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-04-10 12:02:18 +0530

    Updates

---
+++ precache-manifest.b003bbc6aaf44d943cbf11af6a24ea81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a27bd9019784addcb9c1b64787506c",
+    "url": "/quiz-manager/index.html"
+  },
+  {
+    "revision": "b357919bfa7e8b0f4da8",
+    "url": "/quiz-manager/static/css/main.13eea906.chunk.css"
+  },
+  {
+    "revision": "b233cecf8cc99fb5b664",
+    "url": "/quiz-manager/static/js/2.5a507afc.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/quiz-manager/static/js/2.5a507afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b357919bfa7e8b0f4da8",
+    "url": "/quiz-manager/static/js/main.9fdf4968.chunk.js"
+  },
+  {
+    "revision": "e1577ca6f1096e4173be",
+    "url": "/quiz-manager/static/js/runtime-main.ab87f458.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quiz-manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
